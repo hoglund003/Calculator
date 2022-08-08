f11579de9f5204cc14a2cec9a4ce3acf99a71f03
Kasper Høglund <kasper@rubynor.no>
2022-08-08 16:10:24 +0200

    operators added

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function add(num1, num2) {
+    var sum = num1 + num2;
+    return sum;
+}
+
+function subtract(num1, num2) {
+    var sum = num1 - num2;
+    return sum;
+}
+
+function multiply(num1, num2) {
+    var sum = num1 * num2;
+    return sum;
+}
+
+function divide(num1, num2) {
+    var sum = num1 / num2;
+    return sum;
+}
+
+function operate(num1, num2, operator) {
+
+}
