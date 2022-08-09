169dfe20db7844ab0f5fd58ac20a23421479279b
Kasper Høglund <kasper@rubynor.no>
2022-08-09 09:35:48 +0200

    two numbers with operation working

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const expressionEl = document.querySelector(".expression input");
 const numberButtonsEl = document.querySelectorAll(".btn.number");
 const clearButton = document.querySelector(".btn.clear");
 const operatorButonsEl = document.querySelectorAll(".btn.operator");
+const eaqualsButtonEl = document.querySelector(".btn.eaquals");
 
 var expression = "";
+
 
 expressionEl.value = "";
 
@@ -27,8 +29,15 @@ function divide(num1, num2) {
     var sum = num1 / num2;
     return sum;
 }
+const operatorDict = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+};
 
 function operate(num1, num2, operator) {
+    operator = operatorDict[operator]
     var result = operator(num1, num2);
     return result;
 }
@@ -51,8 +60,17 @@ numberButtonsEl.forEach(button => {
 
 operatorButonsEl.forEach(operatorButton => {
     operatorButton.addEventListener("click", () => {
-        addToExpression(operatorButton.innerHTML + " ");
+        addToExpression(" " + operatorButton.innerHTML + " ");
     });
+});
+
+eaqualsButtonEl.addEventListener("click", () => {
+    var expressionArr = expression.split(" ");
+    var num1 = parseInt(expressionArr[0]);
+    var operator = expressionArr[1]
+    var num2 = parseInt(expressionArr[2]);
+    var result = operate(num1, num2, operator);
+    setExpression(result);
 });
 
 clearButton.addEventListener("click", () => {
